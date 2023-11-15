4c31709d7ba5a8e3b65c7c849702735e7ca90c24
tarangchaturvedi <tarang.chaturvedi11@gmail.com>
2023-11-15 20:28:42 +0530

    Update basline_models.py

--- basline_models.py
+++ basline_models.py
@@ -1,90 +1,190 @@
+import pandas as pd
 import math
 ######
-WINDOW_SIZE = 3
-TOTAL_ITERATIONS = 4464 * 40
-
-def moving_average_predictions(ratios, prediction_type):
-    if prediction_type == 'R':
-        predicted_value = ratios['Ratios'].values[0]
-    else:
-        predicted_value = ratios['Prediction'].values[0]
-
-    error = []
-    predicted_values = []
-
-    for i in range(TOTAL_ITERATIONS):
-        if i % 4464 == 0:
+def MA_R_Predictions(ratios,month):
+    predicted_ratio=(ratios['Ratios'].values)[0]
+    error=[]
+    predicted_values=[]
+    window_size=3
+    predicted_ratio_values=[]
+    for i in range(0,4464*40):
+        if i%4464==0:
+            predicted_ratio_values.append(0)
             predicted_values.append(0)
             error.append(0)
             continue
-
-        predicted_values.append(predicted_value)
-
-        if prediction_type == 'R':
-            error.append(abs(math.pow(int(ratios['Given'].values[i] * predicted_value) - ratios['Prediction'].values[i], 1)))
+        predicted_ratio_values.append(predicted_ratio)
+        predicted_values.append(int(((ratios['Given'].values)[i])*predicted_ratio))
+        error.append(abs((math.pow(int(((ratios['Given'].values)[i])*predicted_ratio)-(ratios['Prediction'].values)[i],1))))
+        if i+1>=window_size:
+            predicted_ratio=sum((ratios['Ratios'].values)[(i+1)-window_size:(i+1)])/window_size
         else:
-            error.append(abs(math.pow(predicted_value - ratios['Prediction'].values[i], 1)))
-
-        if i + 1 >= WINDOW_SIZE:
-            sum_values = sum([(j + 1) * ratios[prediction_type].values[i - WINDOW_SIZE + j] for j in range(WINDOW_SIZE)])
-            sum_of_coeff = sum(range(WINDOW_SIZE, 0, -1))
-            predicted_value = int(sum_values / sum_of_coeff)
-        else:
-            sum_values = sum([(j + 1) * ratios[prediction_type].values[j] for j in range(i + 1)])
-            sum_of_coeff = sum(range(i + 1, 0, -1))
-            predicted_value = int(sum_values / sum_of_coeff)
-
-    ratios[f'MA_{prediction_type}_Predicted'] = predicted_values
-    ratios[f'MA_{prediction_type}_Error'] = error
-    mape_err = (sum(error) / len(error)) / (sum(ratios['Prediction'].values) / len(ratios['Prediction'].values))
-    mse_err = sum([e ** 2 for e in error]) / len(error)
+            predicted_ratio=sum((ratios['Ratios'].values)[0:(i+1)])/(i+1)
+            
     
-    return ratios, mape_err, mse_err
-
-###############################################################################################
-
-def exponential_average_predictions(ratios, prediction_type, alpha):
-    predicted_value = ratios['Ratios'].values[0] if prediction_type == 'R' else ratios['Prediction'].values[0]
-
-    error = []
-    predicted_values = []
-
-    for i in range(TOTAL_ITERATIONS):
-        if i % 4464 == 0:
+    ratios['MA_R_Predicted'] = predicted_values
+    ratios['MA_R_Error'] = error
+    mape_err = (sum(error)/len(error))/(sum(ratios['Prediction'].values)/len(ratios['Prediction'].values))
+    mse_err = sum([e**2 for e in error])/len(error)
+    return ratios,mape_err,mse_err
+#############################################
+def MA_P_Predictions(ratios,month):
+    predicted_value=(ratios['Prediction'].values)[0]
+    error=[]
+    predicted_values=[]
+    window_size=1
+    predicted_ratio_values=[]
+    for i in range(0,4464*40):
+        predicted_values.append(predicted_value)
+        error.append(abs((math.pow(predicted_value-(ratios['Prediction'].values)[i],1))))
+        if i+1>=window_size:
+            predicted_value=int(sum((ratios['Prediction'].values)[(i+1)-window_size:(i+1)])/window_size)
+        else:
+            predicted_value=int(sum((ratios['Prediction'].values)[0:(i+1)])/(i+1))
+            
+    ratios['MA_P_Predicted'] = predicted_values
+    ratios['MA_P_Error'] = error
+    mape_err = (sum(error)/len(error))/(sum(ratios['Prediction'].values)/len(ratios['Prediction'].values))
+    mse_err = sum([e**2 for e in error])/len(error)
+    return ratios,mape_err,mse_err
+################################################################################
+def WA_R_Predictions(ratios,month):
+    predicted_ratio=(ratios['Ratios'].values)[0]
+    alpha=0.5
+    error=[]
+    predicted_values=[]
+    window_size=5
+    predicted_ratio_values=[]
+    for i in range(0,4464*40):
+        if i%4464==0:
+            predicted_ratio_values.append(0)
             predicted_values.append(0)
             error.append(0)
             continue
-
-        predicted_values.append(predicted_value)
-
-        if prediction_type == 'R':
-            error.append(abs(math.pow(int(ratios['Given'].values[i] * predicted_value) - ratios['Prediction'].values[i], 1)))
+        predicted_ratio_values.append(predicted_ratio)
+        predicted_values.append(int(((ratios['Given'].values)[i])*predicted_ratio))
+        error.append(abs((math.pow(int(((ratios['Given'].values)[i])*predicted_ratio)-(ratios['Prediction'].values)[i],1))))
+        if i+1>=window_size:
+            sum_values=0
+            sum_of_coeff=0
+            for j in range(window_size,0,-1):
+                sum_values += j*(ratios['Ratios'].values)[i-window_size+j]
+                sum_of_coeff+=j
+            predicted_ratio=sum_values/sum_of_coeff
         else:
-            error.append(abs(math.pow(predicted_value - ratios['Prediction'].values[i], 1)))
+            sum_values=0
+            sum_of_coeff=0
+            for j in range(i+1,0,-1):
+                sum_values += j*(ratios['Ratios'].values)[j-1]
+                sum_of_coeff+=j
+            predicted_ratio=sum_values/sum_of_coeff
+            
+    ratios['WA_R_Predicted'] = predicted_values
+    ratios['WA_R_Error'] = error
+    mape_err = (sum(error)/len(error))/(sum(ratios['Prediction'].values)/len(ratios['Prediction'].values))
+    mse_err = sum([e**2 for e in error])/len(error)
+    return ratios,mape_err,mse_err
+#################################################################
+def WA_P_Predictions(ratios,month):
+    predicted_value=(ratios['Prediction'].values)[0]
+    error=[]
+    predicted_values=[]
+    window_size=2
+    for i in range(0,4464*40):
+        predicted_values.append(predicted_value)
+        error.append(abs((math.pow(predicted_value-(ratios['Prediction'].values)[i],1))))
+        if i+1>=window_size:
+            sum_values=0
+            sum_of_coeff=0
+            for j in range(window_size,0,-1):
+                sum_values += j*(ratios['Prediction'].values)[i-window_size+j]
+                sum_of_coeff+=j
+            predicted_value=int(sum_values/sum_of_coeff)
 
-        predicted_value = alpha * predicted_value + (1 - alpha) * ratios[prediction_type].values[i]
-
-    ratios[f'EA_{prediction_type}_Predicted'] = predicted_values
-    ratios[f'EA_{prediction_type}_Error'] = error
-    mape_err = (sum(error) / len(error)) / (sum(ratios['Prediction'].values) / len(ratios['Prediction'].values))
-    mse_err = sum([e ** 2 for e in error]) / len(error)
+        else:
+            sum_values=0
+            sum_of_coeff=0
+            for j in range(i+1,0,-1):
+                sum_values += j*(ratios['Prediction'].values)[j-1]
+                sum_of_coeff+=j
+            predicted_value=int(sum_values/sum_of_coeff)
     
-    return ratios, mape_err, mse_err
-############################################################################################
+    ratios['WA_P_Predicted'] = predicted_values
+    ratios['WA_P_Error'] = error
+    mape_err = (sum(error)/len(error))/(sum(ratios['Prediction'].values)/len(ratios['Prediction'].values))
+    mse_err = sum([e**2 for e in error])/len(error)
+    return ratios,mape_err,mse_err
+#################################################
 
-def baseline_models():
-    mean_err=[0]*5
-    median_err=[0]*5
-    ratios,mean_err[0],median_err[0]=moving_average_predictions(ratios,'R')
-    ratios,mean_err[1],median_err[1]=moving_average_predictions(ratios,'P')
-    ratios,mean_err[2],median_err[2]=exponential_average_predictions(ratios,'R',0.5)
-    ratios,mean_err[3],median_err[3]=exponential_average_predictions(ratios,'P',0.5)
+def EA_R1_Predictions(ratios,month):
+    predicted_ratio=(ratios['Ratios'].values)[0]
+    alpha=0.6
+    error=[]
+    predicted_values=[]
+    predicted_ratio_values=[]
+    for i in range(0,4464*40):
+        if i%4464==0:
+            predicted_ratio_values.append(0)
+            predicted_values.append(0)
+            error.append(0)
+            continue
+        predicted_ratio_values.append(predicted_ratio)
+        predicted_values.append(int(((ratios['Given'].values)[i])*predicted_ratio))
+        error.append(abs((math.pow(int(((ratios['Given'].values)[i])*predicted_ratio)-(ratios['Prediction'].values)[i],1))))
+        predicted_ratio = (alpha*predicted_ratio) + (1-alpha)*((ratios['Ratios'].values)[i])
+    
+    ratios['EA_R1_Predicted'] = predicted_values
+    ratios['EA_R1_Error'] = error
+    mape_err = (sum(error)/len(error))/(sum(ratios['Prediction'].values)/len(ratios['Prediction'].values))
+    mse_err = sum([e**2 for e in error])/len(error)
+    return ratios,mape_err,mse_err
+################################################################
 
-    ##########
+def EA_P1_Predictions(ratios,month):
+    predicted_value= (ratios['Prediction'].values)[0]
+    alpha=0.3
+    error=[]
+    predicted_values=[]
+    for i in range(0,4464*40):
+        if i%4464==0:
+            predicted_values.append(0)
+            error.append(0)
+            continue
+        predicted_values.append(predicted_value)
+        error.append(abs((math.pow(predicted_value-(ratios['Prediction'].values)[i],1))))
+        predicted_value =int((alpha*predicted_value) + (1-alpha)*((ratios['Prediction'].values)[i]))
+    
+    ratios['EA_P1_Predicted'] = predicted_values
+    ratios['EA_P1_Error'] = error
+    mape_err = (sum(error)/len(error))/(sum(ratios['Prediction'].values)/len(ratios['Prediction'].values))
+    mse_err = sum([e**2 for e in error])/len(error)
+    return ratios,mape_err,mse_err
+############################################################
+
+def baseline_models(smoothed_frames):
+    #Preparing the Dataframe only with x(i) values as jan-2015 data and y(i) values as jan-2016
+    ratios = pd.DataFrame()
+    ratios['Given'] = smoothed_frames['jan_2015']
+    ratios['Prediction'] = smoothed_frames['jan_2016']
+    ratios['Ratios']=ratios['Prediction']*1.0/ratios['Given']*1.0
+
+
+    mean_err=[0]*10
+    median_err=[0]*10
+    ratios,mean_err[0],median_err[0]=MA_R_Predictions(ratios,'jan')
+    ratios,mean_err[1],median_err[1]=MA_P_Predictions(ratios,'jan')
+    ratios,mean_err[2],median_err[2]=WA_R_Predictions(ratios,'jan')
+    ratios,mean_err[3],median_err[3]=WA_P_Predictions(ratios,'jan')
+    ratios,mean_err[4],median_err[4]=EA_R1_Predictions(ratios,'jan')
+    ratios,mean_err[5],median_err[5]=EA_P1_Predictions(ratios,'jan')
+
     print ("Error Metric Matrix (Forecasting Methods) - MAPE & MSE")
     print ("--------------------------------------------------------------------------------------------------------")
     print ("Moving Averages (Ratios) -                             MAPE: ",mean_err[0],"      MSE: ",median_err[0])
     print ("Moving Averages (2016 Values) -                        MAPE: ",mean_err[1],"       MSE: ",median_err[1])
     print ("--------------------------------------------------------------------------------------------------------")
-    print ("Exponential Moving Averages (Ratios) -              MAPE: ",mean_err[2],"      MSE: ",median_err[2])
-    print ("Exponential Moving Averages (2016 Values) -         MAPE: ",mean_err[3],"      MSE: ",median_err[3])
+    print ("Weighted Moving Averages (Ratios) -                    MAPE: ",mean_err[2],"      MSE: ",median_err[2])
+    print ("Weighted Moving Averages (2016 Values) -               MAPE: ",mean_err[3],"      MSE: ",median_err[3])
+    print ("--------------------------------------------------------------------------------------------------------")
+    print ("Exponential Moving Averages (Ratios) -              MAPE: ",mean_err[4],"      MSE: ",median_err[4])
+    print ("Exponential Moving Averages (2016 Values) -         MAPE: ",mean_err[5],"      MSE: ",median_err[5])
